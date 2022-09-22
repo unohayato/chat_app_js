ff67f3aadd005aa5e482ce8e513f1eeb6a6a249e
unohayato <mamidori1p1p@gmail.com>
2022-09-22 20:20:28 +0900

    接続

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
-const io = require("socket.io")(server);
+const { Server } = require("socket.io");
+const io = new Server(server);
 const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("ユーザが接続しました");
 });
 
 server.listen(PORT, () => {
