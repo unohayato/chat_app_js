2120989d6dc1759293cb7ddf83c9ee2b215afb54
unohayato <mamidori1p1p@gmail.com>
2022-09-22 20:05:06 +0900

    Hello World

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Workd");
+});
+
+server.listen(PORT, () => {
+  console.log("listening on 3000");
+});
